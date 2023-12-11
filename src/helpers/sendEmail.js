25adc4630dfbfe85a0f8ea966421b9b60f77247a
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-12-11 17:55:05 -0300

    Update sendEmail.js

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -17,7 +17,7 @@ const emailVerifyAccount = async (datos) => {
   });
 
 
-  const url = `${process.env.URL_CONFIRM}${encodeURIComponent[token]}?token=${encodeURIComponent(token)}`;
+  const url = `https://fish-bold-frontend-nitsugasamot.vercel.app/[token]?token=${encodeURIComponent(token)}`;
 
 
   const info = await transport.sendMail({
